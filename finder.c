f24e80916404b4e0fd5f29c1ff2f6196892c8b9f
bavonmbav <18mt469@esisalama.org>
2022-08-29 10:31:41 +0200

    langage c

--- finder.c
+++ finder.c
@@ -3,128 +3,112 @@
 #include<stdlib.h>
 #include<string.h>
 
-// /*
-// int main(void)
-// {
-//     // printf("bonjour le monde vous allez bien");
-//     // FILE *fp;
-//     // char ch;
-//     // fp = fopen("programme.txt","r");// le deuxiem paramettre specifie le mode d'ouverture d'un fichier
-//     // while (1)
-//     // {
-//     //     ch = fgetc(fp);// lit un caractere du fichier
-//     //     //fseek place  le pointeur du fichier a une position donnee
-//     //     //fputw ecrit un entier dans un fichier
-//     //     //fgetw lit un entier dans un fichier
-//     //     //ftell renvoie la position actuelle
-//     //     //rewind definit le pointeur de fichier au debut du fichier
-//     //     //fopen ouvre un fichier qui prend en paramettre un pointeur comme nom du fichier et une constante pointeur
 
-//     //     if(ch == EOF)
-//     //     break;
-//     //     printf("%c", ch);
-//     // }
-//     // fclose(fp);
-
-// /*
-//     FILE *fp;
-//     fp = fopen("file.txt","w");
-//     fprintf(fp,"bonjour le monde"); //ecrire dans un fichier en mode ecriture
-//     fclose(fp);
-// */
-
-// /*
-// FILE *fp;
-// char caracter[255];
-
-// fp = fopen("file.txt", "r");
-// while (fscanf(fp, "%s", caracter) != EOF)
-// {
-//     printf("%s", caracter);
-// }
-//    fclose(fp);
-//         getch() pour obtenir
-// */
-
-// //===============fputs=====
-
-// // FILE *fp;
-// // fp = fopen("file.txt","w");
-// // fputs("bonjour le langage c",fp);
-// // fclose(fp);
-// // getch();
-
-
-// //===============fgets=====
-// /*
-// FILE *fp;
-// char file[300];
-// fp=fopen("file.txt","r");
-// printf("%s",fgets(file,200,fp));
-// fclose(fp);
-// getch();*/
-// //====================================
-
-
-// //=========================fseek====
-// /*
-//     il existe trois constantes qui sont utiliser pour la fonction fseek
-//     SEEK_SET,SEEK_CUR et SEEK_END
-// */
-// /*
-//     FILE *fp;
-//     fp = fopen("file.txt", "w+");
-//     fputs("bonjour",fp);
-
-//     fseek(fp,20,SEEK_SET);
-//     fputs("ce ci est un tp",fp);
-//     fclose(fp);
-// */
-
-// //========rewind()====================
-
-//    /*
-//     FILE *fp;
-//     char c;
-//     fp =fopen("file.txt","r");
-//     while ((c=fgetc(fp)) != EOF)
-//     {
-//         printf("%c", c);
-//     }
-//     rewind(fp);
-
-//     fclose(fp);
-//     getch();
-// */
-
-// //===================ftell==============
-
-//     FILE *fp;
-//     int tail;
-//     fp = fopen("file.txt", "r");
-//     fseek(fp,0,SEEK_CUR);
-
-//     tail = ftell(fp);
-//     fclose(fp);
-//     printf("la taille du fichier est %d octets", tail);
-//     getch();
-
-//     return 0;
-// }
-
-// exemple de la recupperaton d'un chemin d'acces 
-int main(int argc, char *argv[])
+int main(void)
 {
+    printf("bonjour le monde vous allez bien");
+    FILE *fp1;
+    char ch;
+    fp1 = fopen("programme.txt","r");// le deuxiem paramettre specifie le mode d'ouverture d'un fichier
+    while (1)
+    {
+        ch = fgetc(fp1);// lit un caractere du fichier
+        //fseek place  le pointeur du fichier a une position donnee
+        //fputw ecrit un entier dans un fichier
+        //fgetw lit un entier dans un fichier
+        //ftell renvoie la position actuelle
+        //rewind definit le pointeur de fichier au debut du fichier
+        //fopen ouvre un fichier qui prend en paramettre un pointeur comme nom du fichier et une constante pointeur
 
-    char *chemin;
-    int i;
-    i = 12 ;
+        if(ch == EOF)
+        break;
+        printf("%c", ch);
+    }
+    fclose(fp1);
 
-    chemin = (char*)malloc(sizeof (char) * strlen(argv[0] - i));
 
-    strcpy(chemin, argv[0]);
+    FILE *fp2;
+    fp2 = fopen("file.txt","w");
+    fprintf(fp2,"bonjour le monde"); //ecrire dans un fichier en mode ecriture
+    fclose(fp2);
 
-    strcat(chemin, argv[1]);
-    
+
+
+FILE *fp3;
+char caracter[255];
+
+fp3 = fopen("file.txt", "r");
+while (fscanf(fp3, "%s", caracter) != EOF)
+{
+    printf("%s", caracter);
+}
+   fclose(fp3);
+        getch()// pour obtenir
+
+
+//===============fputs=====
+
+FILE *fp4;
+fp4 = fopen("file.txt","w");
+fputs("bonjour le langage c",fp4);
+fclose(fp4);
+getch();
+
+
+//===============fgets=====
+
+FILE *fp5;
+char file[300];
+fp5=fopen("file.txt","r");
+printf("%s",fgets(file,200,fp5));
+fclose(fp5);
+getch();
+//====================================
+
+
+//=========================fseek====
+/*
+    il existe trois constantes qui sont utiliser pour la fonction fseek
+    SEEK_SET,SEEK_CUR et SEEK_END
+*/
+
+    FILE *fp6;
+    fp6 = fopen("file.txt", "w+");
+    fputs("bonjour",fp6);
+
+    fseek(fp6,20,SEEK_SET);
+    fputs("ce ci est un tp",fp6);
+    fclose(fp6);
+
+
+//========rewind()====================
+
+   
+    FILE *fp7;
+    char c;
+    fp7 =fopen("file.txt","r");
+    while ((c=fgetc(fp7)) != EOF)
+    {
+        printf("%c", c);
+    }
+    rewind(fp7);
+
+    fclose(fp7);
+    getch();
+
+
+//===================ftell==============
+
+    FILE *fp8;
+    int tail;
+    fp8 = fopen("file.txt", "r");
+    fseek(fp8,0,SEEK_CUR);
+
+    tail = ftell(fp8);
+    fclose(fp8);
+    printf("la taille du fichier est %d octets", tail);
+    getch();
+
     return 0;
 }
+
